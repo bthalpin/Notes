efa0587a9bba4238668f83c339353f6cb3817e97
Brian Halpin <brian.t.halpin@gmail.com>
2022-03-14 23:03:31 -0400

    Code cleanup

--- server.js
+++ server.js
@@ -2,53 +2,53 @@ const express = require('express');
 const path = require('path')
 const app = express();
 const PORT = 3001;
-const data = require('./db/db.json');
-
 const fs = require('fs');
-let newData;
 
-    const usuableData = fs.readFileSync('./db/db.json','utf8')
-    newData=JSON.parse(usuableData)
-    let ID = fs.readFileSync('./db/id.json','utf8')
-    id=JSON.parse(ID)
 
-// console.log(usuableData,newData)
+const data = fs.readFileSync('./db/db.json','utf8')
+const ID = fs.readFileSync('./db/id.json','utf8')
+
+
+let notesArray=JSON.parse(data)
+let noteCount=JSON.parse(ID)
+
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+
+
 app.get('/',(req,res)=>{
     res.json('Connected')
     console.log('Get request received')
 })
 
-
 app.get('/notes',(req,res)=>{
     res.sendFile(path.join(__dirname,'public/notes.html'))
 })
+
 app.get('/api/notes',(req,res)=>{
-    res.json(newData)
+    res.json(notesArray)
 })
 
 app.post('/api/notes',(req,res)=>{
-    // console.log(usuableData)
-    req.body.id=id.id
-    id.id++
-    newData.push(req.body)
-    fs.writeFileSync('./db/id.json',JSON.stringify(id),err=>{
+    req.body.id=noteCount.id
+    noteCount.id++
+    notesArray.push(req.body)
+    fs.writeFileSync('./db/id.json',JSON.stringify(noteCount),err=>{
         err?console.log(err):console.log('Updated database')
     })
-    fs.writeFileSync('./db/db.json',JSON.stringify(newData),err=>{
+    fs.writeFileSync('./db/db.json',JSON.stringify(notesArray),err=>{
         err?console.log(err):console.log('Updated database')
     })
 })
 
 app.delete(`/api/notes/:toDelete`,(req,res)=>{
-    newData = newData.filter(data=>data.id!==parseInt(req.params.toDelete))
-    // console.log('delete',reducedData,req.params.toDelete,reducedData[1].id===parseInt(req.params.toDelete))
-    fs.writeFileSync('./db/db.json',JSON.stringify(newData),err=>{
+    notesArray = notesArray.filter(data=>data.id!==parseInt(req.params.toDelete))
+    fs.writeFileSync('./db/db.json',JSON.stringify(notesArray),err=>{
         err?console.log(err):console.log('Updated database')
     })
 })
+
 app.listen(PORT,()=>{
     console.log(`Listening on Port ${PORT}`)
 })
